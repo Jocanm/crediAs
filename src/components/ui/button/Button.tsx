3549195cad24c57f3b638ed7c81558cd96629ef5
Jose Angarita <amjose09@gmail.com>
2023-10-06 13:21:36 -0500

    agregando loader a botones

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,14 +1,30 @@
 import cn from "@/utils/cn/cn";
 import React from "react";
+import { Loader } from "../loader/Loader";
 
-type Props = React.ButtonHTMLAttributes<HTMLButtonElement>;
+type Props = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  isLoading?: boolean;
+};
 
-export const Button: React.FC<Props> = (props) => {
+export const Button: React.FC<Props> = ({
+  isLoading = false,
+  disabled = false,
+  children,
+  ...props
+}) => {
   const baseClassName = cn(
     "bg-primary text-white rounded-2xl py-4 px-10 w-fit",
     props.className,
-    { "bg-primary-light": props.disabled },
+    { "bg-primary-light": disabled || isLoading },
   );
 
-  return <button {...props} className={baseClassName} />;
+  return (
+    <button
+      {...props}
+      className={baseClassName}
+      disabled={disabled || isLoading}
+    >
+      {isLoading ? <Loader /> : children}
+    </button>
+  );
 };
