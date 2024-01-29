f5af2213cec595816721e8b3e109b103f41e20a2
Jose Angarita <jose.angarita@inchcape.com>
2024-01-29 14:52:47 -0500

    corrección

--- src/pages/congrats.tsx
+++ src/pages/congrats.tsx
@@ -15,7 +15,7 @@ const CongratsPage = () => {
     window.open(
       url,
       "_blank",
-      "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400",
+      // "toolbar=yes,scrollbars=yes,resizable=yes,top=500,left=500,width=400,height=400",
     );
   };
 
